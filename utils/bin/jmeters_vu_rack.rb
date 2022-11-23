2232ba911b3b1058f110644083395af2dbb795a0
b08x <rwpannick@gmail.com>
2022-11-23 15:54:56 -0500

    added notes, works well

--- utils/bin/jmeters_vu_rack.rb
+++ utils/bin/jmeters_vu_rack.rb
@@ -4,33 +4,18 @@
 require 'tty-cursor'
 require 'tty-prompt'
 
+def forkoff(command)
+  fork do
+    exec(command.to_s)
+  end
+end
+
 def prompt
   prompt = TTY::Prompt.new(interrupt: :exit)
   return prompt
 end
 
-# connections = `jack_lsp -p`.split("\n")
-#
-# outputs = []
-#
-#
-# connections.each_slice(2) do |pair|
-#   if pair[0].match?(/midi|Midi/) || pair[1].include?("input")
-#     next
-#   end
-#   outputs << pair[0]
-# end
-#
-# puts "#{outputs}"
-#
-# aliases = `jack_lsp -A`.split("\n")
-# aliases.delete_if {|x| x.match?(/midi|Midi|a2j/) }
-#
-# aliases.each_slice(2) do |pair|
-#   puts "#{pair[0]} = #{pair[1]}"
-# end
-
-ports = %w[
+outputs = %w[
   LinuxSampler:0
   LinuxSampler:1
   LinuxSampler:2
@@ -49,69 +34,102 @@ ports = %w[
   LinuxSampler:15
 ]
 
-#########################################
 aliases = []
 
-# get current aliases for linuxsampler ports
+monitor_ports = {}
+
+# get current aliases for linuxsampler outputs
 current_aliases = `jack_lsp -A | grep LinuxSampler -A3`.split(/LinuxSampler/, -1)
 
+# remove midi ports and empty indexes
+current_aliases.delete_if {|x| x.include?("midi") || x.empty? }
+
 current_aliases.each do |line|
-  # formating the output of jack_lsp
+
+  # format the output of jack_lsp
+  # each port that has an alias is followed by an indented line
+  # this is what is returned
+  # [
+  #     [ 0] ":0\n   ngrand\n",
+  #     [ 1] ":1\n   ngrand\n",
+  #     [ 2] ":2\n   contrabass\n",
+  #     [ 3] ":3\n   contrabass\n",
+  #     [ 4] ":4\n",
+  #     [ 5] ":5\n",
+  #     [ 6] ":6\n",
+  #     [ 7] ":7\n",
+  #     [ 8] ":8\n",
+  #     [ 9] ":9\n",
+  #     [10] ":10\n",
+  #     [11] ":11\n",
+  #     [12] ":12\n",
+  #     [13] ":13\n",
+  #     [14] ":14\n",
+  #     [15] ":15\n"
+  # ]
+  #
+  # result for each item
+  # [
+  #     [0] ":0",
+  #     [1] "ngrand"
+  # ]
   alias_array = line.gsub(/\n/,'').split(/\s/).delete_if { |x| x.empty? }
 
-  # if there is only 1 or less objects, then there is no current alias
+  # if the length of the array is less than or equal to 1,
+  # then there is no alias
   next if alias_array.length <= 1
 
-  # select the first alias
+  # select the first alias assigned
   port = alias_array.slice!(0).prepend("LinuxSampler")
-  current_alias = alias_array.slice(0)
+  current_alias = alias_array.slice!(0)
 
-  # if there is more than one, discard the rest
-  alias_array.each do |remaining|
-    `jack_alias -u #{port} #{remaining}`
+  # if there is more than one, unalias the remaining
+  unless alias_array.empty? || alias_array.nil?
+    alias_array.each do |remaining|
+      `jack_alias -u #{port} #{remaining}`
+    end
   end
 
   # ask to use current alias
   use_current = prompt.yes?("use current alias? #{port} = #{current_alias}")
 
-  # if not, unlias if so add to alias array
-  # and remove the port from the port array
+  # if not, unlias
+  # if so add to monitor_ports hash
+  # and remove the port from the outputs array
   if use_current
-    #ports.delete_if { |x| x == port }
-    aliases << current_alias
+    monitor_ports[port] = current_alias
+    outputs.delete_if { |x| x == port }
   else
     `jack_alias -u #{port} #{current_alias}`
   end
 
 end
 
-
 #########################################
 
 puts TTY::Cursor.clear_screen
-#
 
-selections = prompt.multi_select("select ports to assign alias", ports + aliases, per_page: 16)
+# select outputs that currently are not assigned an alias
+choices = prompt.multi_select("select outputs to assign alias", outputs, per_page: 16)
 
+choices.each do |port|
+  # prompt input for new alias
+  new_alias = prompt.ask("Instrument name for port #{port}:  ")
 
-selections.each do |port|
-
-  new_alias = prompt.ask("Instrument name: ")
-
-  aliases << new_alias
-
+  # assign the alias
   `jack_alias #{port} #{new_alias}`
 
-end
-#
-p selections
-p aliases
-test = Hash[ports.zip aliases]
+  # add to monitor_ports hash
+  monitor_ports[port] = new_alias
 
-p test
-#`jmeters -t vu -c 2 #{aliases.join(" ")}`
+end
+
+#
+# "LinuxSampler:0 ngrand LinuxSampler:1 ngrand LinuxSampler:2 contrabass LinuxSampler:3 contrabass LinuxSampler:4 jazzbass"
+
+forkoff("jmeters -t vu -c 3 #{monitor_ports.flatten.join(' ')}")
 
 
 ##################
 
-label = prompt.ask("Instrument name: ")
+#label = prompt.ask("Instrument name: ")
