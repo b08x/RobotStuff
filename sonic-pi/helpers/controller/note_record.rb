3d5664a8a3e93a845eebf51b1e4ef75699ef51ba
b08x <rwpannick@gmail.com>
2023-01-03 07:27:39 -0500

    started note_record

---
+++ sonic-pi/helpers/controller/note_record.rb
@@ -0,0 +1,26 @@
+# Welcome to Sonic Pi
+this_event = 0
+last_event = 0
+notes = []
+
+live_loop :note_record do
+
+  onoff = sync "/osc*/group35/*"
+
+  message = parse_sync_address("/osc*/*/*")
+
+  note = message[2].to_i
+
+  print note
+
+  this_event = beat
+
+  notes << note
+  notes << this_event - last_event
+
+  print notes
+  last_event = this_event
+
+
+
+end
