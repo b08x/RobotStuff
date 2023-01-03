ac8a9bbb643f517b0e25fb30558dc404a329795f
b08x <rwpannick@gmail.com>
2023-01-03 10:44:25 -0500

    added midiseq

--- sonic-pi/helpers/controller/sequencer_v2.rb
+++ sonic-pi/helpers/controller/sequencer_v2.rb
@@ -7,23 +7,23 @@ end
 
 class Sequencer
   attr_accessor :seq1, :seq2, :seq3, :seq4, :midiseq1
-
+  
   def initialize
-
+    
     @seq1 = Array.new(16).map!{|x|x ?x:0}
     @seq2 = Array.new(16).map!{|x|x ?x:0}
     @seq3 = Array.new(16).map!{|x|x ?x:0}
     @seq4 = Array.new(16).map!{|x|x ?x:0}
     @midiseq1 = Array.new(8).map!{|x|x ?x:0}
-
+    
   end
-
+  
   def generate(array,bool,index)
     array.delete_at(index)
     array.insert(index,bool)
     return array
   end
-
+  
   def generate_midi(array,note,index)
     if index == "reset"
       array = Array.new(8).map!{|x|x ?x:0}
@@ -33,26 +33,26 @@ class Sequencer
     end
     return array
   end
-
+  
 end
 
 @sequence = Sequencer.new
 
 def seq_router
   live_loop :router do
-
+    
     use_real_time
-
+    
     onoff = sync "/osc*/*/*"
-
+    
     message = parse_sync_address("/osc*/*/*")
-
+    
     print onoff
     print message
-
+    
     row = message[1]
     print row
-
+    
     # this will be in a range from 1-16
     if message[2] == "reset"
       index = "reset"
@@ -61,9 +61,9 @@ def seq_router
       index = message[2].to_i
       set :index, index - 1
     end
-
+    
     set :onoff, onoff[0]
-
+    
     case row
     when "seq1"
       seq = @sequence.generate(@sequence.seq1 ,get[:onoff], get[:index])
@@ -84,23 +84,23 @@ def seq_router
     when "midiseq1"
       midiseq = @sequence.generate_midi(@sequence.midiseq1 ,get[:onoff], get[:index])
       print midiseq
-
+      
       set row.to_sym, midiseq
     end
-
+    
   end
-
+  
 end
 
 seq_router
 
 live_loop :test do
-
+  
   notes = get[:midiseq1].ring
-
+  
   midi notes.tick, velocity: 90, channel: 1
   sleep 1
-
+  
 end
 
 live_loop :hey do
