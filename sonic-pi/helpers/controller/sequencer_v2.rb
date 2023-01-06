048307f357a96c635299f8e663aa9f61cba49fd1
b08x <rwpannick@gmail.com>
2023-01-06 16:25:30 -0500

    commmented out live_loops

--- sonic-pi/helpers/controller/sequencer_v2.rb
+++ sonic-pi/helpers/controller/sequencer_v2.rb
@@ -7,7 +7,7 @@ end
 
 class Sequencer
   attr_accessor :pattern1, :pattern2, :pattern3, :pattern4, :notes1
-  
+
   def initialize
     @pattern1 = Array.new(16).map!{|x|x ?x:false}
     @pattern2 = Array.new(16).map!{|x|x ?x:false}
@@ -15,16 +15,16 @@ class Sequencer
     @pattern4 = Array.new(16).map!{|x|x ?x:false}
     @notes1 = Array.new(8).map!{|x|x ?x:0}
   end
-  
+
 end
 
 @sequencer = Sequencer.new
 
 if get[:pattern1].nil?
-  set :pattern1, @sequencer.seq1
-  set :pattern2, @sequencer.seq2
-  set :pattern3, @sequencer.seq3
-  set :pattern4, @sequencer.seq4
+  set :pattern1, @sequencer.pattern1
+  set :pattern2, @sequencer.pattern2
+  set :pattern3, @sequencer.pattern3
+  set :pattern4, @sequencer.pattern4
 end
 
 if get[:notes1].nil?
@@ -34,17 +34,17 @@ end
 
 def start_router
   live_loop :router do
-    
+
     use_real_time
     # this will be either 1 or 0
     value = sync "/osc*/*/*"
-    
+
     message = parse_sync_address("/osc*/*/*")
     print message
-    
+
     row = message[1]
     print row
-    
+
     # this will be in a range from 1-16
     if message[2] == "reset"
       index = "reset"
@@ -53,12 +53,12 @@ def start_router
       index = message[2].to_i
       set :index, index - 1
     end
-    
+
     print get[:index]
-    
+
     set :value, value[0]
     print get[:value]
-    
+
     case row
     when "pattern1"
       seq = $generate_sequence[@sequencer.pattern1 ,get[:value], get[:index]]
@@ -85,30 +85,30 @@ def start_router
         set :notes1, notes
       end
     end
-    
+
   end
-  
+
 end
 
-start_router
-
-uncomment do
-  
-  live_loop :test do
-    print get[:notes1].ring
-    
-    notes = get[:notes1].ring
-    
-    midi notes.tick, velocity: 90
-    sleep 0.25
-    
-  end
-  
-  live_loop :hey do
-    
-    sample SOUNDS, "RC_Kick_01", 6, amp: 0.5 if get[:pattern1].ring.tick
-    #play 48, amp: 1.0, attack: 0.125, release: 0.125, on: @pattern1.ring.look
-    sleep 0.25
-  end
-  
-end
+# start_router
+#
+# uncomment do
+#
+#   live_loop :test do
+#     print get[:notes1].ring
+#
+#     notes = get[:notes1].ring
+#
+#     midi notes.tick, velocity: 90
+#     sleep 0.25
+#
+#   end
+#
+#   live_loop :hey do
+#
+#     sample SOUNDS, "RC_Kick_01", 6, amp: 0.5 if get[:pattern1].ring.tick
+#     #play 48, amp: 1.0, attack: 0.125, release: 0.125, on: @pattern1.ring.look
+#     sleep 0.25
+#   end
+#
+# end
