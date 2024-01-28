ff09ceb994eaeabdc0c893c8dd78c3ce976ec699
b08x <rwpannick@gmail.com>
2024-01-28 15:17:18 -0500

    added backup exclude

--- bin/rsyncd.sh
+++ bin/rsyncd.sh
@@ -11,7 +11,7 @@ if [[ -d $backupdelta ]]; then
 
 	rsync -rtPp --log-file=rsync.log --stats --delete --delete-before --delete-excluded --progress --ignore-existing -u -l -b -i -s \
 		--suffix="_backup" --backup-dir=$backupdelta/$timestampe \
-		--exclude-from=/home/b08x/backup_exclude.txt /home/b08x/Public/ /mnt/bender/backup/Public/
+		--exclude-from=/home/b08x/RobotStuff/backup_exclude.txt /home/b08x/Public/ /mnt/bender/backup/Public/
 
 	chown -R b08x:b08x $backupdelta/$timestampe
 else
