457422753c6f977f3a1259de47e0276868b7bc97
b08x <rwpannick@gmail.com>
2024-01-28 14:34:44 -0500

    adjusted conditional

--- bin/rsyncd.sh
+++ bin/rsyncd.sh
@@ -3,10 +3,17 @@
 export timestampe=$(date +'%Y-%m-%d_%H-%M-%S')
 echo $timestampe
 
-mkdir -pv /mnt/bender/storage/backupdelta/$timestampe
+backupdelta="/mnt/usb/backup/backupdelta"
 
-rsync -avPrtv --log-file=rsync.log --stats --delete --delete-before --progress --ignore-existing -u -l -b -i -s \
-	--suffix=$timestampe --backup-dir=/mnt/bender/storage/backupdelta/$timestampe \
-	--exclude-from=/home/b08x/backup_exclude.txt /home/b08x/Public /mnt/bender/backup/
+if [[ -d $backupdelta ]]; then
 
-chown -R b08x:b08x /mnt/bender/storage/backupdelta/$timestampe
+	mkdir -pv $backupdelta/$timestampe
+
+	rsync -rtPp --log-file=rsync.log --stats --delete --delete-before --delete-excluded --progress --ignore-existing -u -l -b -i -s \
+		--suffix="_backup" --backup-dir=$backupdelta/$timestampe \
+		--exclude-from=/home/b08x/backup_exclude.txt /home/b08x/Public/ /mnt/bender/backup/Public/
+
+	chown -R b08x:b08x $backupdelta/$timestampe
+else
+	echo "no usb drive mounted. exiting"
+fi
